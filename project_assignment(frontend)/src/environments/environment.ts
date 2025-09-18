f87733784ae1deb40bd4cf60a4eaa58b1fb8aeff
smoothcoode <laghataieb@gmail.com>
2025-09-18 20:18:05 +0100

    adding deployment

--- project_assignment(frontend)/src/environments/environment.ts
+++ project_assignment(frontend)/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    BASE_HOST:"http://104.155.150.91/api",
+    BASE_HOST:"http://104.155.150.91/api", //change it to your vm` public ip address 
     PRODUCTION:true
 };
